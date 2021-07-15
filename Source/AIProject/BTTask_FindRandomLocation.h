446223365c33590664c43406e13d184bc2aa2bbd
rzisoso <rzisoso@163.com>
2021-07-15 18:22:49 +0800

    Add SightConfig_Cpp_Temp

---
+++ Source/AIProject/BTTask_FindRandomLocation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_FindRandomLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT_API UBTTask_FindRandomLocation : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
