2a4c08de67fed0e2640b1fc7e931b74770d957e1
rzisoso <rzisoso@163.com>
2021-07-18 17:25:56 +0800

    FinishAll_Cpp_Temp

---
+++ Source/AIProject/BTService_UpdateSpeed.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BTService_UpdateSpeed.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT_API UBTService_UpdateSpeed : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AIProject/TestBlackboard.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "TestBlackboard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT_API UTestBlackboard : public UBlackboardData
+{
+	GENERATED_BODY()
+	
+};
