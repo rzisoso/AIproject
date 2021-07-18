25cef1fd213bd3d004a98d7e5e998565ed3b95c5
rzisoso <rzisoso@163.com>
2021-07-18 13:04:49 +0800

    OneDevSecondPatrol_1.0_Cpp_Temp

---
+++ Source/AIProject/BTDecorator_Patrol.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_Patrol.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT_API UBTDecorator_Patrol : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AIProject/MyBTTask_FindPlayerLocation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "MyBTTask_FindPlayerLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT_API UMyBTTask_FindPlayerLocation : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
